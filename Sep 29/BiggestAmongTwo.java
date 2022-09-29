a05cca5df49a60431ae3b04a74b618a1ef797c95
sandeep983 <sandeeppatel983@hotmail.com>
2022-09-29 17:59:47 +0530

    update

---
+++ Sep 29/BiggestAmongTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class BiggestAmongTwo
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>=b)
+            System.out.println(a + " is biggest number");
+        else
+            System.out.println(b + " is biggest number");
+    }
+}
---
+++ Sep 29/SmallAmongTwoDouble.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SmallAmongTwoDouble
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if(a<=b)
+            System.out.println(a + " is the smallest number");
+        else
+            System.out.println(b + " is the smallest number");
+    }
+}
