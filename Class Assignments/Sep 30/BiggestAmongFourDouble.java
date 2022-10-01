e9f319679b450c27aa351d3d595141c969f53caa
sandeep983 <sandeeppatel983@hotmail.com>
2022-10-01 19:32:11 +0530

    update

---
+++ Class Assignments/Sep 30/BiggestAmongFourDouble.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BiggestAmongFourDouble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter four decimal numbers: ");
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+
+        double big = a;
+        if(b > big)
+            big = b;
+        if(c > big)
+            big = c;
+        if(d > big)
+            big = d;
+        
+        System.out.println(big + " is the biggest number.");
+    }
+}
---
+++ Programming Book/Chapter 2/Q9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two integer number: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        sc.close();
+
+        if(n1 > n2)
+            System.out.println(n1);
+        else 
+            System.out.println(n2);
+    }
+}
