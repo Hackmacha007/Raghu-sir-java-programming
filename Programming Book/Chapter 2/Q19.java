9a591d320c51d83c6d29c8f797d11c935639c707
sandeep983 <sandeeppatel983@hotmail.com>
2022-10-02 22:30:21 +0530

    update

--- Programming Book/Chapter 2/Q19.java
+++ Programming Book/Chapter 2/Q19.java
@@ -4,28 +4,28 @@ public class Q19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three integer number: ");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
         sc.close();
 
-        if(n1==n2 || n1==n3 || n2==n3)
+        if(a==b || a==c || b==c)
             System.out.println("Please enter distinct values");
-        else if(n1>n2 && n1>n3)
-            if(n2>n3)
-                System.out.println(n2);
+        else if(a>b && a>c)
+            if(b>c)
+                System.out.println(b);
             else
-                System.out.println(n3);
-        else if(n2>n1 && n2>n3)
-            if(n1>n3)
-                System.out.println(n1);
+                System.out.println(c);
+        else if(b>a && b>c)
+            if(a>c)
+                System.out.println(a);
             else
-                System.out.println(n3);
-        else if(n3>n1 && n3>n2)
-            if(n1>n2)
-                System.out.println(n1);
+                System.out.println(c);
+        else if(c>a && c>b)
+            if(a>b)
+                System.out.println(a);
             else
-                System.out.println(n2);
+                System.out.println(b);
     }
 }
