0550f9c13385b5198389e4cd6a38d277dde76a03
sandeep983 <sandeeppatel983@hotmail.com>
2022-10-09 23:58:42 +0530

    update

--- Programming Book/Chapter 3/Q20.java
+++ Programming Book/Chapter 3/Q20.java
@@ -1,11 +1,43 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Q20 {
-//    public static void main(String[] args) {
-//     System.out.println("Hello");
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
+public class Q20 {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
 
-//         if(int i=0; i<=n; i++)
-//    }
-// }
+
+        int a=0; //first number of series
+        int b=1; //second number of series
+        int c=0; //temporary variable
+
+        if(n < 1)
+        {
+            System.out.println("Invalid input");
+        }
+        else if(n == 1)
+        {
+            System.out.print(a+ " ");
+        }
+        else if(n == 2)
+        {
+            System.out.print(a+ " " + b);
+        }
+        else
+        {   
+            System.out.print(a+ " " + b + " ");
+            while(true)
+            {
+                c = a+b;
+                if(c > n)
+                {
+                    break;
+                }
+                System.out.print(c+" ");
+                a=b;
+                b=c;
+            }
+        }
+    }
+}
