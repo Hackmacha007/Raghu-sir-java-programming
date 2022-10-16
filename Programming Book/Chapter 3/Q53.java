78c92b7dd90bb4dc870ab04ccf0ec57e605b0d69
sandeep983 <sandeeppatel983@hotmail.com>
2022-10-16 14:59:19 +0530

    update

--- Programming Book/Chapter 3/Q53.java
+++ Programming Book/Chapter 3/Q53.java
@@ -2,12 +2,12 @@ public class Q53 {
     public static void main(String[] args) {
         int arr[] = {10, 20, 30, 40, 50};
 
-        System.out.println("Working of for loop");
+        System.out.println("Working of for-each / enhanced for loop");
         for(int i: arr) {
             System.out.print(i+" ");
         }
 
-        System.out.println("\n\nWorking of for-each loop");
+        System.out.println("\n\nWorking of for loop");
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
