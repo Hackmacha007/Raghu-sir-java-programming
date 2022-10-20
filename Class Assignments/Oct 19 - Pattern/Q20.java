e26f6d9bd3eaf0626770caa2b4becca4208cba88
sandeep983 <sandeeppatel983@hotmail.com>
2022-10-20 18:10:02 +0530

    update

---
+++ Class Assignments/Oct 19 - Pattern/Q20.java
@@ -0,0 +1,19 @@
+/*
+Print this pattern:
+        1               i=1      j=1 j<=i    j%2 -> 1 / i%2 -> 1
+      0 1 0               2    
+    1 0 1 0 1             3
+  0 1 0 1 0 1 0           4
+1 0 1 0 1 0 1 0 1              */
+
+import java.util.Scanner;
+public class Q20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of lines: ");
+        int n = sc.nextInt();
+        sc.close();
+
+       
+    } 
+}
---
+++ Class Assignments/Oct 19 - Pattern/Q21.java
@@ -0,0 +1,19 @@
+/*
+Print this pattern:
+        1
+      1 * 2
+    1 * 2 * 3 
+  1 * 2 * 3 * 4
+1 * 2 * 3 * 4 * 5                */
+
+import java.util.Scanner;
+public class Q21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of lines: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        
+    } 
+}
