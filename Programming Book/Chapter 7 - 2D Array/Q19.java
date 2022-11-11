e4a0146108b054b0c546e578eefb0971bd0d2980
sandeep983 <sandeeppatel983@hotmail.com>
2022-11-11 23:21:34 +0530

    update

--- Programming Book/Chapter 7 - 2D Array/Q19.java
+++ Programming Book/Chapter 7 - 2D Array/Q19.java
@@ -1,13 +1,13 @@
 //Q19. WAJP to Sort the Matrix in Ascending order and Descending order.
 
 public class Q19 extends ArrayMethods {
-    private static int[][] ascendingOrder(int[][] matrix) {
+    private static int[][] ascendingOrder(int[][] matrix) {   
         int[][] ascOrder = new int[matrix.length][matrix[0].length];
 
         //need to write the logic - bubble sort
 
         return ascOrder;
-    } 
+    }
 
     private static int[][] descendingOrder(int[][] matrix) {
         int[][] descOrder = new int[matrix.length][matrix[0].length];
