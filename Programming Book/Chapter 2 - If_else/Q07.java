ff1fb576f0d644f06b99971cf6a63c021a510b21
sandeep983 <sandeeppatel983@hotmail.com>
2022-11-16 13:33:45 +0530

    update

--- Programming Book/Chapter 2 - If_else/Q07.java
+++ Programming Book/Chapter 2 - If_else/Q07.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Q07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-
         sc.close();
 
         if(n>0) {
